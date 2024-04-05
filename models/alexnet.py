b97335430a790c626c3df0b7ee6ceb8f0cb05d45
timothygao8710 <timothygao8710@gmail.com>
2024-04-04 22:54:11 -0700

    jdf;lk

--- models/alexnet.py
+++ models/alexnet.py
@@ -41,7 +41,7 @@ class AlexNet(nn.Module):
         
         self.classifier = nn.Sequential(
             nn.Dropout(),
-            nn.Linear(6 * 6, 4096),
+            nn.Linear(3 * 6 * 6, 4096),
             nn.ReLU(inplace=True),
             nn.Dropout(),
             nn.Linear(4096, 4096),
